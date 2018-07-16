01340c4ea92a62a38589ee85ac56b103e0258dcc
saiminhtet <htetsaimin@gmail.com>
2018-07-16 13:23:15 +0800

    created business logic directory

---
+++ Group8AD_WebAPI/BusinessLogic/DepartmentBL.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Group8AD_WebAPI.BusinessLogic
+{
+    public class DepartmentBL
+    {
+    }
+}
