e3d319941a0e1de576978bd8ba583b1c61d0d1bd
Noel <dnnoelsue90@gmail.com>
2018-07-16 23:36:17 +0800

    Updated BusinessLogic by Supplier

--- Group8AD_WebAPI/BusinessLogic/SupplierBL.cs
+++ Group8AD_WebAPI/BusinessLogic/SupplierBL.cs
@@ -3,19 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Group8AD_WebAPI.Models;
+using Group8AD_WebAPI.Models.ViewModels;
 
 namespace Group8AD_WebAPI.BusinessLogic
 {
     public static class SupplierBL
     {
         //get AllSupplier list 
-        //public static List<SupplierLists> GetAllSupp(string itemCode)
-        //{
-        //    using (SA46Team08ADProjectContext entities = new SA46Team08ADProjectContext())
-        //    {
-        //        List<SupplierList> suppList = entities.SupplierList.ToList<SupplierList>();
-        //        return suppList;
-        //    }
-        //}
+        public static List<SupplierVM> GetAllSupp(string itemCode)
+        {
+            List<SupplierVM> supplists = new List<SupplierVM>();
+
+            using (SA46Team08ADProjectContext entities = new SA46Team08ADProjectContext())
+            {
+                supplists = entities.Suppliers.Select(s => new SupplierVM()
+                {
+                    SuppCode = s.SuppCode,
+                    SuppName = s.SuppName,
+                    SuppCtcName = s.SuppCtcName,
+                    SuppCtcNo = s.SuppCtcNo,
+                    SuppFaxNo = s.SuppFaxNo,
+                    SuppAddr = s.SuppAddr
+                }).ToList<SupplierVM>();
+            }
+            return supplists;
+        }
+        //SupplierList GetAllSupp
+        public static List<SupplierVM> GetAllSupp()
+        {
+            List<SupplierVM> supplists = new List<SupplierVM>();
+
+            using (SA46Team08ADProjectContext entities = new SA46Team08ADProjectContext())
+            {
+                supplists = entities.Suppliers.Select(s => new SupplierVM()
+                {
+                    SuppCode = s.SuppCode,
+                    SuppName = s.SuppName,
+                    SuppCtcName = s.SuppCtcName,
+                    SuppCtcNo = s.SuppCtcNo,
+                    SuppFaxNo = s.SuppFaxNo,
+                    SuppAddr = s.SuppAddr
+                }).ToList<SupplierVM>();
+            }
+            return supplists;
+        }
     }
 }
