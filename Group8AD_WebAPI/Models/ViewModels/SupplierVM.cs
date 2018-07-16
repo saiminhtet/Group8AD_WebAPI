92b8668217d7c31444cdf95f12253cd8e25bcfcb
Noel <dnnoelsue90@gmail.com>
2018-07-16 23:34:56 +0800

    Update ViewModel for Supplier

---
+++ Group8AD_WebAPI/Models/ViewModels/SupplierVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Group8AD_WebAPI.Models.ViewModels
+{
+    public class SupplierVM
+    {
+        public string SuppCode { get; set; }
+        public string SuppName { get; set; }
+        public string SuppCtcName { get; set; }
+        public string SuppCtcNo { get; set; }
+        public string SuppFaxNo { get; set; }
+        public string SuppAddr { get; set; }
+    }
+}
