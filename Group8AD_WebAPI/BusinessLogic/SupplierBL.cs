7df5215b046973463aedfda03f173ff960fbc206
Noel <dnnoelsue90@gmail.com>
2018-07-16 19:24:13 +0800

    updated DepartmentBL

---
+++ Group8AD_WebAPI/BusinessLogic/SupplierBL.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Group8AD_WebAPI.Models;
+
+namespace Group8AD_WebAPI.BusinessLogic
+{
+    public static class SupplierBL
+    {
+        //get AllSupplier list 
+        public static List<SupplierList> GetAllSupp(string itemCode)
+        {
+            using (SA46Team08ADProjectContext entities = new SA46Team08ADProjectContext())
+            {
+                List<SupplierList> suppList = entities.SupplierList.ToList<SupplierList>();
+                return suppList;
+            }
+        }
+    }
+}
