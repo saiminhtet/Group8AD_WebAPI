e87a82d2a3191781d7ec4c598034bc291a31baf2
tang_shenqi <tang_shenqi@outlook.com>
2018-07-17 16:02:50 +0800

    update adjustment controller

--- Group8AD_WebAPI/Controllers/AdjustmentController.cs
+++ Group8AD_WebAPI/Controllers/AdjustmentController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Group8AD_WebAPI.BusinessLogic;
+using Group8AD_WebAPI.Models;
+using Group8AD_WebAPI.Models.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -10,6 +13,69 @@ namespace Group8AD_WebAPI.Controllers
     //Controllers
     public class AdjustmentController : ApiController
     {
+        [Route("api/adjustment/add")]
+        public HttpResponseMessage AddAdjustment(Adjustment adj)
+        {
+            AdjustmentVM adjustment = AdjustmentBL.AddAdj(adj);
+            if (adjustment == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError);
+            }
+            return Request.CreateResponse(HttpStatusCode.OK, adjustment);
+        }
+
+        [AcceptVerbs("GET")]
+        [HttpGet]
+        [Route("api/adjustment/{voucherNo}")]
+        public HttpResponseMessage GetAdjustment(string voucherNo)
+        {
+            AdjustmentVM adjustment = AdjustmentBL.GetAdj(voucherNo);
+            if (adjustment == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError);
+            }
+            return Request.CreateResponse(HttpStatusCode.OK, adjustment);
+        }
+
+        [AcceptVerbs("GET")]
+        [HttpGet]
+        [Route("api/adjustment/{status}")]
+        public HttpResponseMessage GetAdjustmentList(string status)
+        {
+            List<AdjustmentVM> adjlist = AdjustmentBL.GetAdjList(status);
+            if (adjlist == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError);
+            }
+            return Request.CreateResponse(HttpStatusCode.OK, adjlist);
+        }
+
+        [Route("api/adjustment/raise")]
+        public HttpResponseMessage RaiseAdjustment(int empId, List<AdjustmentVM> iList)
+        {
+            List<AdjustmentVM> adjlist = AdjustmentBL.RaiseAdjustments(empId, iList);
+            if (adjlist == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError);
+            }
+            return Request.CreateResponse(HttpStatusCode.OK, adjlist);
+        }
+
+        [Route("api/adjustment/accept")]
+        public HttpResponseMessage AcceptRequest(string voucherNo, int empId, string cmt)
+        {
+            AdjustmentBL.AcceptRequest(voucherNo, empId, cmt);
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+
+        [Route("api/adjustment/rejct")]
+        public HttpResponseMessage RejectRequest(string voucherNo, int empId, string cmt)
+        {
+            AdjustmentBL.RejectRequest(voucherNo, empId, cmt);
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+
+
         // GET api/<controller>
         public IEnumerable<string> Get()
         {
