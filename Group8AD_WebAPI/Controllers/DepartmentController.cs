dc236ebdb9ca21f72a2dfc328324e4015ecb4c22
Noel <dnnoelsue90@gmail.com>
2018-07-17 18:42:26 +0800

    updated Controller

--- Group8AD_WebAPI/Controllers/DepartmentController.cs
+++ Group8AD_WebAPI/Controllers/DepartmentController.cs
@@ -4,36 +4,135 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Group8AD_WebAPI.Models;
+using Group8AD_WebAPI.Models.ViewModels;
 
 namespace Group8AD_WebAPI.Controllers
 {
     public class DepartmentController : ApiController
     {
-        // GET api/<controller>
-        public IEnumerable<string> Get()
+        //removeDelegate
+        [System.Web.Http.AcceptVerbs("POST")]
+        [System.Web.Http.HttpPost]
+        [Route("api/Department/{deptCode}/removeDelegate")]
+        public HttpResponseMessage removeDelegate(string deptCode)
         {
-            return new string[] { "value1", "value2" };
+            DepartmentVM department = BusinessLogic.DepartmentBL.removeDelegate(deptCode);
+
+            if (department == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError);
+            }
+            return Request.CreateResponse(HttpStatusCode.OK, department);
         }
 
-        // GET api/<controller>/5
-        public string Get(int id)
+        //set Delegate by DepartmentCode , fromDate , toDate and empId
+        [System.Web.Http.AcceptVerbs("POST")]
+        [System.Web.Http.HttpPost]
+        [Route("api/Department/{deptCode}/setDelegate")]
+        public HttpResponseMessage setDelegate(string deptCode, DateTime fromDate, DateTime toDate, int empId)
         {
-            return "value";
+            DepartmentVM department = BusinessLogic.DepartmentBL.setDelegate(deptCode,fromDate,toDate,empId);
+
+            if (department == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError);
+            }
+            return Request.CreateResponse(HttpStatusCode.OK, department);
         }
 
-        // POST api/<controller>
-        public void Post([FromBody]string value)
+        //set Rep by DepartmentCode , fromEmpId and toEmpId
+        [System.Web.Http.AcceptVerbs("POST")]
+        [System.Web.Http.HttpPost]
+        [Route("api/Department/{deptCode}/setRep")]
+        public HttpResponseMessage setRep(string deptCode, int fromEmpId, int toEmpId)
         {
+            DepartmentVM department = BusinessLogic.DepartmentBL.setRep(deptCode, fromEmpId, toEmpId);
+
+            if (department == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError);
+            }
+            return Request.CreateResponse(HttpStatusCode.OK, department);
         }
 
-        // PUT api/<controller>/5
-        public void Put(int id, [FromBody]string value)
+        //removeRep(string deptCode, int fromEmpId)
+        [System.Web.Http.AcceptVerbs("POST")]
+        [System.Web.Http.HttpPost]
+        [Route("api/Department/{deptCode}/removeRep")]
+        public HttpResponseMessage removeRep(string deptCode, int fromEmpId)
         {
+            DepartmentVM department = BusinessLogic.DepartmentBL.removeRep(deptCode, fromEmpId);
+
+            if (department == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError);
+            }
+            return Request.CreateResponse(HttpStatusCode.OK, department);
         }
 
-        // DELETE api/<controller>/5
-        public void Delete(int id)
+        //setCollPt(string deptCode, int collPt)
+        [System.Web.Http.AcceptVerbs("POST")]
+        [System.Web.Http.HttpPost]
+        [Route("api/Department/{deptCode}/removeRep")]
+        public HttpResponseMessage setCollPt(string deptCode, int collPt)
         {
+            DepartmentVM department = BusinessLogic.DepartmentBL.setCollPt(deptCode, collPt);
+
+            if (department == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError);
+            }
+            return Request.CreateResponse(HttpStatusCode.OK, department);
+        }
+
+        //GetCollPtList
+        //tested
+        [System.Web.Http.AcceptVerbs("GET")]
+        [System.Web.Http.HttpGet]
+        [Route("api/Department/GetCollectionPoint")]
+        public HttpResponseMessage GetCollPtList()
+        {
+            List<string> departments = BusinessLogic.DepartmentBL.GetCollPtList();
+
+            if (departments == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError);
+            }
+            return Request.CreateResponse(HttpStatusCode.OK, departments);
+        }
+
+        //GetCollPt
+        //tested
+        [System.Web.Http.AcceptVerbs("GET")]
+        [System.Web.Http.HttpGet]
+        [Route("api/Department/{deptCode}")]
+        public HttpResponseMessage GetCollPt(string deptCode)
+        {
+
+            string colPt = BusinessLogic.DepartmentBL.GetCollPt(deptCode);
+
+            if (colPt == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError);
+            }
+            return Request.CreateResponse(HttpStatusCode.OK, colPt);
+        }
+
+        //List<string> GetDeptCodes()
+        //tested
+        [System.Web.Http.AcceptVerbs("GET")]
+        [System.Web.Http.HttpGet]
+        [Route("api/Department/GetDeptCodes")]
+        public HttpResponseMessage GetDeptCodes()
+        {
+            List<string> departments = BusinessLogic.DepartmentBL.GetDeptCodes();
+
+            if (departments == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError);
+            }
+            return Request.CreateResponse(HttpStatusCode.OK, departments);
         }
     }
 }
---
+++ Group8AD_WebAPI/Controllers/NotificationController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Group8AD_WebAPI.Models;
+using Group8AD_WebAPI.Models.ViewModels;
+
+namespace Group8AD_WebAPI.Controllers
+{
+    public class NotificationController : ApiController
+    {
+        //addNewReqNotification
+        [System.Web.Http.AcceptVerbs("POST")]
+        [System.Web.Http.HttpPost]
+        [Route("api/Notification/{empId}/{currReq}/addNoti")]
+        public HttpResponseMessage AddNewReqNotification(int empId, int currReq)
+        {
+            NotificationVM noti = BusinessLogic.NotificationBL.AddNewReqNotification(empId, currReq);
+
+            if (noti == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError);
+            }
+            return Request.CreateResponse(HttpStatusCode.OK, noti);
+        }
+
+        //List<NotificationVM> GetNotifications(int empId)
+        //tested
+        [System.Web.Http.AcceptVerbs("GET")]
+        [System.Web.Http.HttpGet]
+        [Route("api/Notifications/{empId}")]
+        public HttpResponseMessage GetNotifications(int empId)
+        {
+            List<NotificationVM> noti = BusinessLogic.NotificationBL.GetNotifications(empId);
+
+            if (noti == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError);
+            }
+            return Request.CreateResponse(HttpStatusCode.OK, noti);
+        }
+
+        //NotificationVM NotifyManager(Notification n)
+        //tested
+        [System.Web.Http.AcceptVerbs("GET")]
+        [System.Web.Http.HttpGet]
+        [Route("api/Notification/StoreManager")]
+        public HttpResponseMessage NotifyManager(NotificationVM n)
+        {
+            NotificationVM notification = BusinessLogic.NotificationBL.NotifyManager(n);
+
+            if (notification == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError);
+            }
+            return Request.CreateResponse(HttpStatusCode.OK, notification);
+        }
+
+        //NotificationVM NotifySupervisor(Notification n)
+        //tested
+        [System.Web.Http.AcceptVerbs("GET")]
+        [System.Web.Http.HttpGet]
+        [Route("api/Notification/Supervisor")]
+        public HttpResponseMessage NotifySupervisor(NotificationVM n)
+        {
+            NotificationVM notification = BusinessLogic.NotificationBL.NotifySupervisor(n);
+
+            if (notification == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError);
+            }
+            return Request.CreateResponse(HttpStatusCode.OK, notification);
+        }
+    }
+}
