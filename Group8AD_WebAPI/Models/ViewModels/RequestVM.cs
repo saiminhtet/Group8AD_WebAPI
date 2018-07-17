f9ff9eddc7a8809fac5dbe3266767dbdc4a56d11
tang_shenqi <tang_shenqi@outlook.com>
2018-07-17 13:52:03 +0800

    update RequesrBL

---
+++ Group8AD_WebAPI/Models/ViewModels/RequestVM.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Group8AD_WebAPI.Models.ViewModels
+{
+    public class RequestVM
+    {
+        public int ReqId { get; set; }
+        public int EmpId { get; set; }
+        public int ApproverId { get; set; }
+        public string ApproverComment { get; set; }
+        public DateTime ReqDateTime { get; set; }
+        public DateTime ApprovalDateTime { get; set; }
+        public DateTime CancelledDateTime { get; set; }
+        public DateTime FulfilledDateTime { get; set; }
+        public string Status { get; set; }
+    }
+}
