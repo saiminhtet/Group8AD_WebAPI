3e90009cd0063a6cc58f0280bf59617619274fab
Noel <dnnoelsue90@gmail.com>
2018-07-19 19:47:39 +0800

    Department update

---
+++ Group8AD_WebAPI/Models/ViewModels/CollectionPointVM.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+namespace Group8AD_WebAPI.Models.ViewModels
+{
+    public class CollectionPointVM
+    {
+        public int ColPtId { get; set; }
+        public string Location { get; set; }
+    }
+}
--- Group8AD_WebAPI/Models/ViewModels/DepartmentVM.cs
+++ Group8AD_WebAPI/Models/ViewModels/DepartmentVM.cs
@@ -13,12 +13,9 @@ namespace Group8AD_WebAPI.Models.ViewModels
         public string DeptCtcNo { get; set; }
         public string DeptFaxNo { get; set; }
         public Nullable<int> ColPtId { get; set; }
-        public string Location { get; set; }
         public Nullable<int> DeptHeadId { get; set; }
         public Nullable<int> DeptRepId { get; set; }
         public Nullable<int> DelegateApproverId { get; set; }
-        public DateTime DelegateFromDate { get; set; }
-        public DateTime DelegateToDate { get; set; }
         public int EmpId { get; set; }
 
     }
